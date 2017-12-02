e6aa29fde6ced7f6be1ecc87e82dde41d1679dc5
Mikael Lönneberg <mikael.lonneberg@radicalcode.se>
2017-12-02 10:53:27 +0100

    December first, part2

--- 1/problem1.py
+++ 1/problem1.py
@@ -1,17 +1,24 @@
 from itertools import tee
 from itertools import izip
+from itertools import islice
+from itertools import chain
 from collections import deque
 
-def returnIfEqual(tup):
+def return_if_equal(tup):
     if tup[0] == tup[1]:
-        return tup[0]
+        return int(tup[0])
     return 0
 
-def pairwise(iterable):
-    a, b = tee(iterable)
-    next(b, None)
-    return izip(a, b)
+def pairwise(iterable, offSet):
+    head = islice(iterable, 0, offSet)
+    tail = islice(iterable, offSet, None)
+    return izip(iterable, chain(tail, head))
 
-def circleSum(iter):
-    q = deque(iter)
-    return sum(map(returnIfEqual, pairwise(map(int, iter)))) + returnIfEqual((int(q.pop()), int(q.popleft())))
+def circle_sum(my_input, offSet):
+    return sum(map(return_if_equal, pairwise(my_input, offSet))) 
+
+def circle_sum_next(my_input):
+    return circle_sum(my_input, 1)
+
+def circle_sum_half(my_input):
+    return circle_sum(my_input, len(my_input)/2)
--- 1/test/problem1_test.py
+++ 1/test/problem1_test.py
@@ -1,19 +1,21 @@
 import unittest
-from ..problem1 import returnIfEqual
-from ..problem1 import pairwise
-from ..problem1 import circleSum
+from ..problem1 import *
 
 class TestProblem(unittest.TestCase):
 
     def test_add_if_equal(self):
-        self.assertEqual(returnIfEqual((1, 2)), 0)
-        self.assertEqual(returnIfEqual((1, 1)), 1)
+        self.assertEqual(return_if_equal((1, 2)), 0)
+        self.assertEqual(return_if_equal((1, 1)), 1)
 
     def test_circle_sum(self):
-        self.assertEqual(circleSum("1122"), 3)
-        self.assertEqual(circleSum("1111"), 4)
-        self.assertEqual(circleSum("1234"), 0)
-        self.assertEqual(circleSum("91212129"), 9)
-        
-        
-    
+        self.assertEqual(circle_sum_next("1122"), 3)
+        self.assertEqual(circle_sum_next("1111"), 4)
+        self.assertEqual(circle_sum_next("1234"), 0)
+        self.assertEqual(circle_sum_next("91212129"), 9)
+
+    def test_circle_sum(self):
+        self.assertEqual(circle_sum_half("1212"), 6)
+        self.assertEqual(circle_sum_half("1221"), 0)
+        self.assertEqual(circle_sum_half("123425"), 4)
+        self.assertEqual(circle_sum_half("123123"), 12)    
+        self.assertEqual(circle_sum_half("12131415"), 4)    
