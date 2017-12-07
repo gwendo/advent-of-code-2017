ba7bd3d6c3c541188913159dae09fcddb1b59b4a
Mikael Lönneberg <mlonneberg@cpaglobal.com>
2017-12-07 15:04:19 +0100

    December seventh
    Not very pretty

--- 7/problem7.py
+++ 7/problem7.py
@@ -1,20 +1,67 @@
-
+from collections import Counter
+from itertools import izip
 
 class Prog:
-    def __init__(self, weight, children):
-       self.c = children
-       self.w = weight
-    
+    def __init__(self, name, weight):
+        self.n = name
+        self.p = None
+        self.w = weight
+        self.c = []
+
+    def add_child(self, child):
+        self.c.append(child)
+
+    def add_parent(self, parent):
+        self.p = parent
+
+    def __repr__(self):
+        return "(n: %s )" % (self.n)
+
+
 def parse_prog(param):
     split = param.split(None)
-    p = Prog(int(param[1].trim("()"))), param[0])
+    p = Prog(split[0], int(split[1].strip("()")))
     if len(split) > 2:
-        p.children = split[3].split(",")
+        map(p.add_child, map(lambda x: x.strip(","), split[3:]))
     return p
 
-def find_base(file_name):
+
+def set_parent(p, progs):
+    children = filter(lambda pro: pro.n in p.c, progs)
+    p.c = children
+    for child in children:
+        child.p = p
+
+def is_unbalanced(p):
+    return len(Counter(map(sum_prog, p.c))) > 1
+
+
+def find_weight(p):
+    sums = map(sum_prog, p.c)
+    soloSum = filter(lambda x: x[1] == 1, Counter(sums).items())
+    diff = sums[0] - sums[sums.index(soloSum[0][0])]
+    return p.c[sums.index(soloSum[0][0])].w + diff
+
+def find_unbalanced(progs):
+    unbalanced = filter(is_unbalanced, progs)
+    return find_weight(unbalanced[0])
+
+
+def sum_prog(p):
+    summa = p.w + sum(map( lambda x: x.w if len(x.c) == 0 else sum_prog(x), p.c))
+    return summa
+
+def find_progs(file_name):
     with open(file_name) as testfile:
         content = testfile.readlines()
     progs = map(parse_prog, content)
-    return filter(l)
-    
+    for prog in progs:
+        set_parent(prog, progs)
+    return progs
+
+def find_base(file_name):
+    progs = find_progs(file_name)
+
+    base = filter(lambda p: p.p == None, progs)[0]
+    return base
+
--- 7/test/problem7_test.py
+++ 7/test/problem7_test.py
@@ -1,6 +1,16 @@
 import unittest
+from ..problem7 import *
 
 class Problem7Test(unittest.TestCase):
     
     def test_find_base(self):
-        self.assertEquals(find_base("./7/test/test_input.txt"), "tknk")
+        self.assertEquals(find_base("./7/test/test_input.txt").n, "tknk")
+
+    def test_find_base_sol(self):
+        self.assertEquals(find_base("./7/test/problem7_input.txt").n, "cyrupz")
+
+    def test_find_weight(self):
+        self.assertEquals(find_unbalanced(find_progs("./7/test/test_input.txt")), 60)
+
+    def test_find_weight_sol(self):
+        self.assertEquals(find_unbalanced(find_progs("./7/test/problem7_input.txt")), 193)
