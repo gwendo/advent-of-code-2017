256f68f10568acbff474863d4e03e092fb61f74f
Mikael Lönneberg <mikael.lonneberg@radicalcode.se>
2017-12-07 07:48:34 +0100

    WIP: December seventh

--- 7/problem7.py
+++ 7/problem7.py
@@ -1,11 +1,20 @@
 
 
 class Prog:
-    parent = null
-   def __init__(self, weight, children):
+    def __init__(self, weight, children):
        self.c = children
        self.w = weight
     
+def parse_prog(param):
+    split = param.split(None)
+    p = Prog(int(param[1].trim("()"))), param[0])
+    if len(split) > 2:
+        p.children = split[3].split(",")
+    return p
 
 def find_base(file_name):
+    with open(file_name) as testfile:
+        content = testfile.readlines()
+    progs = map(parse_prog, content)
+    return filter(l)
     
